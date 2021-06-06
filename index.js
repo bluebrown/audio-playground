750b379ec8e99165c624df0cc2080c54bc4b9353
Nico Braun <nico-braun@live.de>
2021-06-06 13:34:01 +0100

    one function to rule them all

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 (async () => {
-    { // main scope
+    // main scope
+    {
+
         const appContext = document.getElementById('app')
         const ctx = new AudioContext()
         window.play = () => ctx.resume()
@@ -29,43 +31,66 @@
 
         const nodes = {}
 
-        {
-            const [uid, node, head] = createOscillator(ctx, dispatchSelection)
-            nodes[uid] = node
-            appContext.append(head)
+        const stolenAnalyserCallback = (ctx, analyser, head) => {
+            const canvas = document.createElement('canvas')
+            head.querySelector('.controls').append(canvas)
+            analyser.fftSize = 2048;
+            var bufferLength = analyser.frequencyBinCount;
+            var dataArray = new Uint8Array(bufferLength);
+            analyser.getByteTimeDomainData(dataArray);
+            var canvasCtx = canvas.getContext("2d");
+            function draw() {
+                requestAnimationFrame(draw);
+                analyser.getByteTimeDomainData(dataArray);
+                canvasCtx.fillStyle = "rgb(200, 200, 200)";
+                canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+                canvasCtx.lineWidth = 2;
+                canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+                canvasCtx.beginPath();
+                var sliceWidth = canvas.width * 1.0 / bufferLength;
+                var x = 0;
+                for (var i = 0; i < bufferLength; i++) {
+                    var v = dataArray[i] / 128.0;
+                    var y = v * canvas.height / 2;
+                    if (i === 0) {
+                        canvasCtx.moveTo(x, y);
+                    } else {
+                        canvasCtx.lineTo(x, y);
+                    }
+                    x += sliceWidth;
+                }
+                canvasCtx.lineTo(canvas.width, canvas.height / 2);
+                canvasCtx.stroke();
+            }
+            draw();
         }
-        {
-            const [uid, node, head] = createOscillator(ctx, dispatchSelection, true)
-            nodes[uid] = node
-            appContext.append(head)
+
+        const lfoOpts = {
+            constructor: { frequency: 0.8 },
+            params: {
+                frequency: [-5.0, 5.0, 0.01],
+            },
+            select: [
+                {
+                    label: 'type',
+                    items: ['sine', 'square', 'sawtooth', 'triangle'],
+                }
+            ]
         }
-        {
-            const [uid, node, head] = createGain(ctx, dispatchSelection)
-            nodes[uid] = node
-            appContext.append(head)
-        }
-        {
-            const [uid, node, head] = createAnalyzer(ctx, dispatchSelection)
-            nodes[uid] = node
-            appContext.append(head)
-        }
-        {
-            const [uid, node, head] = createDelay(ctx, dispatchSelection)
-            nodes[uid] = node
-            appContext.append(head)
-        }
-        {
-            const [uid, node, head] = createStereoPanner(ctx, dispatchSelection)
-            nodes[uid] = node
-            appContext.append(head)
-        }
-        {
-            const [uid, node, head] = createBiquadFilter(ctx, dispatchSelection)
-            nodes[uid] = node
-            appContext.append(head)
-        }
-        {
-            const [uid, node, head] = createCompressor(ctx, dispatchSelection)
+
+        const nodeList = [
+            ['OscillatorNode', OscillatorNode, {}],
+            ['LfoNode', OscillatorNode, lfoOpts],
+            ['GainNode', GainNode, {}],
+            ['StereoPannerNode', StereoPannerNode, {}],
+            ['PannerNode', PannerNode, {}],
+            ['DynamicsCompressorNode', DynamicsCompressorNode, {}],
+            ['BiquadFilterNode', BiquadFilterNode, {}],
+            ['AnalyserNode', AnalyserNode, { callback: stolenAnalyserCallback }],
+        ]
+
+        for (const [label, Constructor, options] of nodeList) {
+            const [uid, node, head] = createNode(label, ctx, Constructor, options, dispatchSelection)
             nodes[uid] = node
             appContext.append(head)
         }
@@ -73,8 +98,8 @@
         const dest = ctx.destination
         const destHead = createHead(dest, 'Destination', [], dispatchSelection)
         appContext.append(destHead)
-    }
 
+    }
 
     function guid() {
         const S4 = () => (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
@@ -83,6 +108,16 @@
 
     function randomColor() {
         return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    }
+
+    function getAudioParams(node) {
+        const params = []
+        for (const propName in node) {
+            if (node[propName] instanceof AudioParam) {
+                params.push(propName)
+            }
+        }
+        return params
     }
 
     function appendVertexBadge(ctx, colo, inGuid, outGuid) {
@@ -96,7 +131,6 @@
 
     function connectSelected(outSelect, inSelect) {
         try {
-            console.log(inSelect.node, outSelect.index, inSelect.index)
             if (inSelect.type == 'param') {
                 outSelect.node.connect(inSelect.node)
             } else {
@@ -115,7 +149,7 @@
 
     function createHead(node, label = '', params = [], dispatchSelection = console.log) {
         const nodeHead = document.createElement('article')
-        nodeHead.guid = guid()
+        nodeHead.id = guid()
         nodeHead.classList = 'node-head'
         const heading = document.createElement('h1')
         heading.textContent = label
@@ -217,7 +251,8 @@
             input.append(opt)
         }
         input.onchange = onchange
-        return input
+        controlGroup.append(input)
+        return controlGroup
     }
 
     function createControlSection() {
@@ -238,167 +273,48 @@
         }
     }
 
-    function createGenericNode(ctx, label, node, params, dispatchSelection) {
+    function createNode(label, ctx, Constructor, options = {}, dispatchSelection = console.log) {
+        // create new node by invoking the constructor
+        const node = new Constructor(ctx, options.constructor || {})
+        const params = getAudioParams(node)
         const head = createHead(node, label, params, dispatchSelection)
+
+        // create control section
         const controlSection = createControlSection()
         head.append(controlSection)
-        for (let p of params) {
-            let min, max, step
-            if (typeof p === 'string') {
-                min = node[p].minValue
-                max = node[p].maxValue
-                step = 0.1
-            } else {
-                min = p[1]
-                max = p[2]
-                step = p[3]
-                p = p[0]
+
+        // add select options of any
+        if (options.select instanceof Array) {
+            for (const { label, items } of options.select) {
+                controlSection.append(selectInput(label, items, ({ target }) => {
+                    node.type = target.value
+                }))
             }
-            controlSection.append(rangeInput(p, node[p].value, min, max, step, ({ target }) => {
+        }
+
+        // add param slider if any
+        for (let p of params) {
+            const ap = node[p]
+            const pOpts = options.params && options.params[p]
+            const min = pOpts ? pOpts[0] : ap.minValue
+            const max = pOpts ? pOpts[1] : ap.maxValue
+            const step = pOpts ? pOpts[3] : 0.1
+            controlSection.append(rangeInput(p, ap.value, min, max, step, ({ target }) => {
                 saveSmoothValueChange(node[p], target.value, ctx.currentTime)
             }))
         }
+
+        if (typeof options.callback === 'function') {
+            options.callback(ctx, node, head)
+        }
+
+        // start the node if required
+        if (typeof node.start === 'function') {
+            node.start()
+        }
+
+        // return the soup
         return [head.id, node, head]
-    }
-
-    function createOscillator(ctx, dispatchSelection, lfo = false) {
-        const osc = ctx.createOscillator()
-        if (lfo) {
-            osc.frequency.value = 0.1
-        }
-        osc.start()
-        const oscHead = createHead(osc, lfo ? 'LFO' : 'Oscillator', ['frequency', 'detune'], dispatchSelection)
-
-        const controlSection = createControlSection()
-        oscHead.append(controlSection)
-
-        controlSection.append(selectInput('type', ['sine', 'square', 'sawtooth', 'triangle'], ({ target }) => {
-            osc.type = target.value
-        }))
-
-        const min = lfo ? -10 : 60
-        const max = lfo ? 10 : 2200
-        const step = lfo ? 0.01 : 0.1
-
-        controlSection.append(rangeInput('frequency', osc.frequency.value, min, max, step, ({ target }) => {
-            saveSmoothValueChange(osc.frequency, target.value, ctx.currentTime)
-
-        }))
-
-        controlSection.append(rangeInput('detune', osc.detune.value, -25, 25, 1, ({ target }) => {
-            saveSmoothValueChange(osc.detune, target.value, ctx.currentTime)
-        }))
-
-        return [oscHead.id, osc, oscHead]
-    }
-
-    function createAnalyzer(ctx, dispatchSelection) {
-        const analyser = ctx.createAnalyser();
-        const head = createHead(analyser, 'Analyzer', [], dispatchSelection)
-        const controlSection = createControlSection()
-        head.append(controlSection)
-        const canvas = document.createElement('canvas')
-        head.append(canvas)
-
-        analyser.fftSize = 2048;
-
-        var bufferLength = analyser.frequencyBinCount;
-        var dataArray = new Uint8Array(bufferLength);
-        analyser.getByteTimeDomainData(dataArray);
-
-        var canvasCtx = canvas.getContext("2d");
-
-        // draw an oscilloscope of the current audio source
-
-        function draw() {
-
-            requestAnimationFrame(draw);
-
-            analyser.getByteTimeDomainData(dataArray);
-
-            canvasCtx.fillStyle = "rgb(200, 200, 200)";
-            canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-            canvasCtx.lineWidth = 2;
-            canvasCtx.strokeStyle = "rgb(0, 0, 0)";
-
-            canvasCtx.beginPath();
-
-            var sliceWidth = canvas.width * 1.0 / bufferLength;
-            var x = 0;
-
-            for (var i = 0; i < bufferLength; i++) {
-
-                var v = dataArray[i] / 128.0;
-                var y = v * canvas.height / 2;
-
-                if (i === 0) {
-                    canvasCtx.moveTo(x, y);
-                } else {
-                    canvasCtx.lineTo(x, y);
-                }
-
-                x += sliceWidth;
-            }
-
-            canvasCtx.lineTo(canvas.width, canvas.height / 2);
-            canvasCtx.stroke();
-        }
-
-        draw();
-
-        return [head.id, analyser, head]
-
-    }
-
-    function createGain(ctx, dispatchSelection) {
-        return createGenericNode(
-            ctx,
-            'Gain',
-            ctx.createGain(),
-            [['gain', 0, 1, 0.1]],
-            dispatchSelection
-        )
-    }
-
-    function createDelay(ctx, dispatchSelection) {
-        return createGenericNode(
-            ctx,
-            'Delay',
-            ctx.createDelay(),
-            ['delayTime'],
-            dispatchSelection
-        )
-    }
-
-    function createStereoPanner(ctx, dispatchSelection) {
-        return createGenericNode(
-            ctx,
-            'Stereo Pan',
-            ctx.createStereoPanner(),
-            ['pan'],
-            dispatchSelection
-        )
-    }
-
-    function createBiquadFilter(ctx, dispatchSelection) {
-        return createGenericNode(
-            ctx,
-            'Biquad Filter',
-            ctx.createBiquadFilter(),
-            ['Q', 'detune', 'frequency', 'gain'],
-            dispatchSelection
-        )
-    }
-
-    function createCompressor(ctx, dispatchSelection) {
-        return createGenericNode(
-            ctx,
-            'Compressor',
-            ctx.createDynamicsCompressor(),
-            ['attack', 'knee', 'ratio', 'release', 'threshold'],
-            dispatchSelection
-        )
     }
 
 })().catch(console.warn)
